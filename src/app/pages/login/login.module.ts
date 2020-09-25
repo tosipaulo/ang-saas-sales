6f80ba77c83b52231a47d21f045f4a6461c0a04c
Paulo Tosi <tosi.paulo@gmail.com>
2020-09-24 23:13:05 -0300

    Criando rota para login

--- src/app/pages/login/login.module.ts
+++ src/app/pages/login/login.module.ts
@@ -1,13 +1,12 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { LoginComponent } from './login.component';
-
-
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { LoginComponent } from "./login.component";
+import { SignUpComponent } from "./sign-up/sign-up.component";
+import { LoginRoutingModule } from "./login-routing.module";
 
 @NgModule({
-  declarations: [LoginComponent],
-  imports: [
-    CommonModule
-  ]
+  declarations: [LoginComponent, SignUpComponent],
+  imports: [CommonModule, LoginRoutingModule],
+  exports: [LoginComponent, SignUpComponent],
 })
-export class LoginModule { }
+export class LoginModule {}
