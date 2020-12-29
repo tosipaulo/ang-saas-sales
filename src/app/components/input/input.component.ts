405445907af808219c3485f4517e1a8545a0dba1
Paulo Tosi <tosi.paulo@gmail.com>
2020-12-29 19:32:17 -0300

    Create form sign login

--- src/app/components/input/input.component.ts
+++ src/app/components/input/input.component.ts
@@ -1,11 +1,14 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, Input } from "@angular/core";
 
 @Component({
   selector: "lib-input",
   templateUrl: "./input.component.html",
-  styleUrls: ["./input.component.scss"],
+  styleUrls: ["./input.component.scss"]
 })
 export class InputComponent implements OnInit {
+
+  @Input() title: string;
+
   constructor() {}
 
   ngOnInit(): void {}
