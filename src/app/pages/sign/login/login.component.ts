ed43cc340c70aaa6764a58f31cea0b33d88db935
Paulo Tosi <tosi.paulo@gmail.com>
2020-12-31 11:03:59 -0300

    Create login without token

--- src/app/pages/sign/login/login.component.ts
+++ src/app/pages/sign/login/login.component.ts
@@ -1,5 +1,10 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+import { SignService } from '../sign.service';
 
 @Component({
   selector: 'app-login',
@@ -11,18 +16,34 @@ export class LoginComponent implements OnInit {
 
   loginForm: FormGroup;
 
-  constructor(private fb: FormBuilder) { }
+  constructor(
+    private fb: FormBuilder,
+    private signService: SignService,
+    private router: Router,
+    private snackBar: MatSnackBar
+    ) { }
 
   ngOnInit(): void {
     this.loginForm = this.fb.group({
       email: ['', [Validators.required, Validators.email]],
-      password: ['', Validators.required],
-      teste: ['', Validators.required]
+      password: ['', Validators.required]
     })
   }
 
   onSubmit() {
-    console.log(this.loginForm.value)
+    if(this.loginForm.valid) {
+      this.signService.login(this.loginForm.value).subscribe(
+        (user) => {
+          this.router.navigate(['clientes']);
+        },
+        (error: HttpErrorResponse) => {
+          console.log(error)
+          this.snackBar.open(`${error['error']['message']}`, '❌', {
+            duration: 3000,
+          });
+        }
+      )
+    }
   }
 
 }
